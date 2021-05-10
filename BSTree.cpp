7be870c4a0bed9209cc6f759431da3db2c69efe5
afaber003 <afaber003@gmail.com>
2021-05-10 15:07:07 -0700

    added main2.cpp for testing

--- BSTree.cpp
+++ BSTree.cpp
@@ -1,81 +1,80 @@
 #include "BSTree.h"
+
 #include <iostream>
 using namespace std;
 
-BSTree::BSTree(){
+BSTree::BSTree() {
     root = nullptr;
 }
 
-void BSTree::insert(const string& newdata){
+void BSTree::insert(const string& newdata) {
     Node* newnode = new Node(newdata);
-    if (root == nullptr){root = newnode; return;} // case for empty Tree
+    if (root == nullptr) { // case for empty Tree
+        root = newnode;
+        return;
+    }  
     Node* curr = root;
-    while (curr->hasChild()){ //this while() only compares to internal nodes
-        if (curr->getData() == newnode->getData()){ //case for data being already in tree
-            curr->increaseCount();
-            delete newnode;
+start:
+    if (newdata == curr->getData()){ // if data is identical
+        curr->increaseCount();
+        return;
+    }
+    if (newdata > curr->getData()){
+        if (curr->getRightChild() != nullptr){ //if its not the bottom of the tree yet
+            curr = curr->getRightChild();
+            goto start;
+        }
+        else {  // if it reached the bottom of the tree
+            curr->setRightChild(newnode);
             return;
         }
-        else if (curr->getData() > newnode->getData()){ //case for bigger than curr
-            curr = curr->getRightChild();
-        }
-        else if (curr->getData() < newnode->getData()){ //case for smaller than curr
+    }
+    if (newdata < curr->getData()){
+        if (curr->getLeftChild() != nullptr){
             curr = curr->getLeftChild();
+            goto start;
         }
-    }
-    if (curr->getData() > newnode->getData()){ //this + next 2 else-if statments are actually setting a new leaf with the new node
-        curr->setLeftChild(newnode); 
-    }
-    else if (curr->getData() < newnode->getData()){ 
-        curr->setRightChild(newnode);
-    }
-    else if (curr->getData() == newnode->getData()){
-        curr->increaseCount();
-        delete newnode;
+        else {
+            curr->setLeftChild(newnode);
+            return;
+        }
     }
 }
 
-void BSTree::remove(const string &key)
-{
-  
+void BSTree::remove(const string& key) {
 }
 
 //dont question this one. its the worst function ive ever written i think
-bool BSTree::search(const string& lookforthis) const{
+bool BSTree::search(const string& lookforthis) const {
     Node* curr = root;
-    while (lookforthis != curr->getData()){
-        if (curr->hasChild() == false){
+    while (lookforthis != curr->getData()) {
+        if (curr->hasChild() == false) {
             return false;
-        }     
-        if (lookforthis > curr->getData()){
+        }
+        if (lookforthis > curr->getData()) {
             curr = curr->getRightChild();
         } else {
             curr = curr->getLeftChild();
         }
-        if (curr == nullptr){
+        if (curr == nullptr) {
             return false;
         }
     }
     return true;
 }
 
-string BSTree::largest() const
-{
-  Node* curr = root;
-  string largestString = ""; 
-  while (curr->rightChildExists() == true)
-  {
-    curr = curr->getRightChild();
-  }
-  largestString = curr->getData();
-  if (curr == nullptr)
-  {
-    largestString = "";
-  }
-  
+string BSTree::largest() const {
+    Node* curr = root;
+    string largestString = "";
+    while (curr->rightChildExists() == true) {
+        curr = curr->getRightChild();
+    }
+    largestString = curr->getData();
+    if (curr == nullptr) {
+        largestString = "";
+    }
 
-
-  return largestString;
+    return largestString;
 }
 
 /*
@@ -86,134 +85,100 @@ string BSTree::largest() const
 
 */
 
-string BSTree::smallest() const
-{
-  Node* curr = root;
-  string smallestString = "";
+string BSTree::smallest() const {
+    Node* curr = root;
+    string smallestString = "";
 
-  while (curr->leftChildExists() == true)
-  {
-    smallestString = curr->getLeftChild()->getData();
-  }
-  smallestString = curr->getData();
-  if (curr == nullptr)
-  {
-    smallestString = "";
-  }
-
-  return smallestString;
+    while (curr->leftChildExists() == true) {
+        smallestString = curr->getLeftChild()->getData();
+    }
+    smallestString = curr->getData();
+    if (curr == nullptr) {
+        smallestString = "";
+    }
+    return smallestString;
 }
 
-int BSTree::height(const string &startString) const
-{
-  Node* startStringNode;
-  int treeHeight = 0;
-  if (search(startString) == false)
-  {
-    return -1;
-  }
-  if (root->getData() == startString)
-  {
-    return 0;
-  }
-  else
-  {
-    //Fix
-    startStringNode = findNode(startString, root);
-    if (startStringNode->leftChildExists() && startStringNode->rightChildExists())
-    {
-      if (startStringNode->getLeftChild()->getCount() > startStringNode->getRightChild()->getCount())
-      {
-        treeHeight = startStringNode->getRightChild()->getCount();
-      }
-    }
-    else if (startStringNode->leftChildExists() && startStringNode->rightChildExists() == false)
-    {
-      treeHeight = startStringNode->getLeftChild()->getCount();
-    }
-    else if (startStringNode->rightChildExists() && startStringNode->leftChildExists() == false)
-    {
-      treeHeight = startStringNode->getRightChild()->getCount();
-    }
-    else
-    {
-      treeHeight = 0;
-    }
-  }
-  
-
-  return treeHeight; 
+int BSTree::height(const string& startString) const {
+    if (!search(startString)){return -1;} // case for string doesnt exist
+    Node* curr = findNode(startString, root);
+    int ans = findHeight(curr); //calls recursive function
+    return ans;
 }
 
-//Put Find Height here
+int BSTree::findHeight(Node* start) const {
+    if (start == nullptr){return 0;}
+    if (start->hasChild() == false){
+        return 0;
+    } else {
+        return 1 + findHeight(start->getLeftChild() + findHeight(start->getRightChild()));
+    }
+}
 
-void BSTree::inOrder() const{
+void BSTree::inOrder() const {
     inOrder(root);
 }
 
-void BSTree::inOrder(Node* start) const{
-    if (start == nullptr){return;}
+void BSTree::inOrder(Node* start) const {
+    if (start == nullptr) {
+        return;
+    }
     inOrder(start->getLeftChild());
-    cout << start->getData() << '(' << start->getCount() << ')'  << endl;
+    cout << start->getData() << '(' << start->getCount() << ')' << endl;
     inOrder(start->getRightChild());
 }
 
-void BSTree::postOrder() const{
+void BSTree::postOrder() const {
     postOrder(root);
 }
 
-void BSTree::postOrder(Node* start) const{
-    if (start == nullptr){return;}
+void BSTree::postOrder(Node* start) const {
+    if (start == nullptr) {
+        return;
+    }
     postOrder(start->getLeftChild());
     postOrder(start->getRightChild());
-    cout << start->getData() << '(' << start->getCount() << ')'  << endl;
+    cout << start->getData() << '(' << start->getCount() << ')' << endl;
 }
 
-void BSTree::preOrder() const{
+void BSTree::preOrder() const {
     preOrder(root);
 }
 
-void BSTree::preOrder(Node* start) const{
-    if (start == nullptr){return;}
-    cout << start->getData() << '(' << start->getCount() << ')'  << endl;
+void BSTree::preOrder(Node* start) const {
+    if (start == nullptr) {
+        return;
+    }
+    cout << start->getData() << '(' << start->getCount() << ')' << endl;
     preOrder(start->getLeftChild());
     preOrder(start->getRightChild());
 }
 
-BSTree::~BSTree(){
+BSTree::~BSTree() {
     deletetraverse(root);
 }
 
-void BSTree::deletetraverse(Node* start){
-    if (start == nullptr){return;}
+void BSTree::deletetraverse(Node* start) {
+    if (start == nullptr) {
+        return;
+    }
     deletetraverse(start->getLeftChild());
     deletetraverse(start->getRightChild());
     delete start;
 }
 
-Node* BSTree::findNode(string nodeData, Node* startNode) const
-{
-  Node* currNode = startNode;
+Node* BSTree::findNode(string nodeData, Node* startNode) const {
+    Node* currNode = startNode;
 
-  while (currNode != nullptr)
-  {
-    if (currNode->getData() == nodeData)
-    {
-      return currNode;
+    while (currNode != nullptr) {
+        if (currNode->getData() == nodeData) {
+            return currNode;
+        } else if (nodeData < currNode->getData()) {
+            currNode = currNode->getLeftChild();
+        } else {
+            currNode = currNode->getRightChild();
+        }
     }
-    else if (nodeData < currNode->getData())
-    {
-      currNode = currNode->getLeftChild();
-    }
-    else
-    {
-      currNode = currNode->getRightChild();
-    }
-  }
 
-  return currNode;
-  
+    return currNode;
 }
-
-
-
--- BSTree.h
+++ BSTree.h
@@ -2,34 +2,33 @@
 #define __BSTREE_H__
 #include "Node.h"
 
-class BSTree
-{
-private:
-   Node *root;
+class BSTree {
+   private:
+    Node *root;
 
-   //helper functions
-   void inOrder(Node*) const;
-   void postOrder(Node*) const;
-   void preOrder(Node*) const;
-   void deletetraverse(Node*);
-   Node* findNode(string, Node*) const;
-   int findHeight(Node*);
+    //helper functions
+    void inOrder(Node *) const;
+    void postOrder(Node *) const;
+    void preOrder(Node *) const;
+    void deletetraverse(Node *);
+    Node *findNode(string, Node *) const;
+    int findHeight(Node *) const;
 
-public:
-   /* Constructors */
-   /* Default constructor */
-   BSTree();
-   ~BSTree();
+   public:
+    /* Constructors */
+    /* Default constructor */
+    BSTree();
+    ~BSTree();
 
-   /* Mutators */
-   /* Insert an item into the binary search tree. 
+    /* Mutators */
+    /* Insert an item into the binary search tree. 
            Be sure to keep BST properties. 
            When an item is first inserted into the tree the count should be set to 1. 
            When adding a duplicate string (case sensitive), rather than adding another node, 
            the count variable should be incremented 
          */
-   void insert(const string &newString);
-   /* Remove a specified string from the tree. 
+    void insert(const string &newString);
+    /* Remove a specified string from the tree. 
            Be sure to maintain all bianry search tree properties. 
            If removing a node with a count greater than 1, just decrement the count, otherwise, 
            if the count is simply 1, remove the node. 
@@ -44,35 +43,31 @@ public:
                 larger than the current string to remove 
                 (i.e. find the smallest value in the right subtree of the node to remove. 
          */
-   void remove(const string &key);
+    void remove(const string &key);
 
-   /* Accessors */
-   /* Search for a string in the binary search tree. 
+    /* Accessors */
+    /* Search for a string in the binary search tree. 
            It should return true if the string is in the tree, and false otherwise. 
          */
-   bool search(const string &key) const;
-   /* Find and return the largest value in the tree. Return an empty string if the tree is empty */
-   string largest() const;
-   /* Find and return the smallest value in the tree. Return an emtpy string if the tree is empty */
-   string smallest() const;
-   /* Compute and return the height of a particular string in the tree. 
+    bool search(const string &key) const;
+    /* Find and return the largest value in the tree. Return an empty string if the tree is empty */
+    string largest() const;
+    /* Find and return the smallest value in the tree. Return an emtpy string if the tree is empty */
+    string smallest() const;
+    /* Compute and return the height of a particular string in the tree. 
            The height of a leaf node is 0 (count the number of edges on the longest path). 
            Return -1 if the string does not exist. 
          */
-   int height(const string &) const;
+    int height(const string &) const;
 
-   /* Printing */
-   /* For all printing orders, each node should be displayed as follows:
+    /* Printing */
+    /* For all printing orders, each node should be displayed as follows:
            <string> (<count>)
            e.g. goodbye(1), Hello World(3)
            */
-   void preOrder() const;
-   void postOrder() const;
-   void inOrder() const;
-
-  
-
-
+    void preOrder() const;
+    void postOrder() const;
+    void inOrder() const;
 };
 
-#endif // __BSTREE_H__
+#endif  // __BSTREE_H__
